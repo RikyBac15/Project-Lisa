bbb781e1727dc4c41869e25d7a521d763f76fa1d
RikyBac15 <riccardo.bacchetti@gmail.com>
2022-01-13 20:29:48 +0100

    Hot word
    Trying to implement wake up word with porcupine but having problems with the execution of the code

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import speech_recognition as sr
 from playsound import playsound
 import python_weather
 import asyncio
+import struct
+import pyaudio
+import pvporcupine
+import pywhatkit
 '''from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
@@ -12,10 +16,16 @@ from kivy.core.window import Window'''
 #import pyttsx3
 import os
 import wikipedia
-
 wikipedia.set_lang("it")
 
-'''
+'''access_key = "nXOstMU6qix+GJqaEpYTNDjoz/nqUoWcPBkU4rWd2voWC7lO3/R3dg=="
+keyword_paths = ['HotWord_model/hey_lisa_linux_v2.0.0/hey-lisa_en_linux_v2_0_0.ppn']
+porcupine = pvporcupine.create(access_key=access_key, keyword_paths=keyword_paths)
+
+pa = pyaudio.PyAudio()
+
+audio_stream = pa.open(rate = 44100, channels = 1, format = pyaudio.paInt16, input = True, frames_per_buffer = 3200)
+
 Window.size = (500, 300)
 Window.borderless = True
 
@@ -49,6 +59,10 @@ async def getweather(date):
     # close the wrapper once done
     await client.close()'''
 
+def get_next_audio_frame():
+
+    pass
+
 def take_command():
 	try:
 		with sr.Microphone(device_index=4) as source:
@@ -56,11 +70,11 @@ def take_command():
 			voice = listener.listen(source)
 			command = listener.recognize_google(voice, language="it-IT")
 			command = command.lower()
-			if 'lisa' in command:
-				playsound('Audio/Lisa_wake-up.mp3')
+			print (command)
+			'''if 'lisa' in command:
+				#playsound('Audio/Lisa_wake-up.mp3')
 				command = command.replace('lisa', '')
-				#speak("I am fine, thank you")
-				print (command)			
+				print (command)'''		
 	except:
 		pass
 	return command
@@ -68,8 +82,7 @@ def take_command():
 def run_lisa():
 	command = take_command()
 	if 'cerca informazioni' in command:
-		argument = command.replace('cerca informazioni su ', '')
-		SearchWiki = wikipedia.suggest(argument)
+		SearchWiki = command.replace('cerca informazioni su ', '')
 		print(wikipedia.summary(SearchWiki, sentences=1))
 	'''if 'che tempo' in command:
 		location = command.replace('che tempo farà ', '')
@@ -80,4 +93,26 @@ def run_lisa():
 			when = 'today'
 		print (getweather(when))'''
 
+def lisa_wake():
+	with sr.Microphone(device_index=4) as source:
+		voice = listener.listen(source)
+		hotword = vosk.KaldiRecognizer(model, args.samplerate)
+		hotword = command.lower()
+
+'''while True:
+    pcm = audio_stream.read(porcupine.frame_length)
+    pcm = struct.unpack_from("h" * porcupine.frame_length, pcm)
+    keyword_index = porcupine.process(pcm)
+    if keyword_index >= 0:
+        playsound('Audio/Lisa_wake-up.mp3')'''
 run_lisa()
+        #pass
+
+if porcupine is not None:
+    porcupine.delete()
+
+if audio_stream is not None:
+    audio_stream.close()
+
+if pa is not None:
+    pa.terminate()
